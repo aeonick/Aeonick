119896f95a00f3cffef004a6560ae63ae5f4ec47
unknown <sky101001@gmail.com>
2016-08-20 17:59:29 +0800

    'edit'

--- run.py
+++ run.py
@@ -2,10 +2,9 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, request, g, flash, send_from_directory
 import sys
-import sqlite3
 import aids
+import sqlite3
 
-#初始化设置
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 app = Flask(__name__)
@@ -13,16 +12,17 @@ DATABASE = os.path.join(app.root_path, 'db.db')
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'foolish'
 app.config.from_object(__name__)
 
-#数据库链接模块
 def get_db():
     if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        g.sqlite_db = connect_db()
     return g.sqlite_db
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    return rv
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -31,12 +31,11 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('error.html'), 500
 
-#笨办法手动加盐验证密码
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         import aids
-        if aids.check(request.form['password']):
+        if aids.check(request.form['namea']):
             session['log'] = True
             return redirect(url_for('index'))
     return render_template('login.html')
@@ -45,15 +44,14 @@ def logout():
     session['log'] = False
     return redirect(url_for('index'))
 
-#博客编辑模块
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if session.get('log'):
         if request.method == 'POST':
             if request.form['editor'] and request.form['title']:
-                abstract=aids.abstr(request.form['editor'])
-                #偷懒写一块了,file即分类编号（历史遗留问题）
-                get_db().execute('insert into blog (title, content, abstract, tag, file) values (?, ?, ?, ?, ?)', (request.form['title'], request.form['editor'], abstract,request.form['tags'],request.form['file'],))
+                abstr=aids.abstr(request.form['editor'])
+                cur=get_db().cursor()
+                cur.execute('insert into blog (title, content, abstract, tag, file) values (?, ?, ?, ?, ?)', (request.form['title'], request.form['editor'], abstr,request.form['tags'],request.form['file'],))
                 get_db().commit()
                 return redirect(url_for('index'))
             elif request.form['editor']:
@@ -61,110 +59,133 @@ def new():
                 return render_template('edit.html',content=request.form['editor'])
         return render_template('edit.html')
     return redirect(url_for('index'))
-@app.route('/edit/<int:blog>', methods=['GET', 'POST'])
-def edit(blog):
+
+@app.route('/edit/<int:bg_id>', methods=['GET', 'POST'])
+def edit(bg_id):
     if session.get('log'):
         try:
-            temp=get_db().execute('SELECT title, content,tag from blog where id=?',(blog,)).fetchall()[0]
+            cur=get_db().cursor()
+            cur.execute('SELECT title, content,tag from blog where id=?',(bg_id,))
+            cont=cur.fetchall()[0]
         except:
             return redirect(url_for('index'))
         if request.method == 'POST':
             if request.form['editor'] and request.form['title']:
-                abstract=aids.abstr(request.form['editor'])
-                get_db().execute('UPDATE blog SET title = ? ,content = ?,abstract=?,tag=? ,file=? WHERE ID = ?;', (request.form['title'], request.form['editor'],abstract,request.form['tags'],request.form['file'], blog))
+                abstr=aids.abstr(request.form['editor'])
+                cur=get_db().cursor()
+                cur.execute('UPDATE blog SET title = ? ,content = ?,abstract=?,tag=? ,file=? WHERE ID = ?;', (request.form['title'], request.form['editor'],abstr,request.form['tags'],request.form['file'], bg_id))
                 get_db().commit()
-                return redirect(url_for('article',blog=blog))
+                return redirect(url_for('article',bg_id=bg_id))
             elif request.form['editor']:
                 flash('请填写标题')
                 return render_template('edit.html',content=request.form['editor'])
-        return render_template('edit.html',content=temp[1],title=temp[0],tags=temp[2])
+        return render_template('edit.html',content=cont[1],title=cont[0],tags=cont[2])
     return redirect(url_for('index'))
 
-
-@app.route('/delb/<int:blog>')
-def del_blog(blog):
+@app.route('/dele/<int:bg_id>')
+def dele(bg_id):
     if session.get('log'):
         try:
-            get_db().execute('DELETE FROM blog WHERE id = ? ',(blog,))
+            cur=get_db().cursor()
+            cur.execute('DELETE FROM blog WHERE id = ? ',(bg_id,))
             get_db().commit()
         except:
             return redirect(url_for('index'))
     return redirect(url_for('index'))
-@app.route('/delc/<int:blog>/<int:c_id>')
-def del_comment(blog,c_id):
-    if session.get('log'):
-        try:
-            get_db().execute('DELETE FROM comm WHERE id = ? ',(c_id,))
-            get_db().commit()
-        finally:
-            if blog == 0 :
-                return redirect(url_for('memo'))
-            else:
-                return redirect(url_for('article',blog=blog))
-    return redirect(url_for('article',blog=blog))
 
 @app.route('/')
 def index():
     return redirect(url_for('page',pg=1))
 
 @app.route('/article/None')
-def back_to_index():
+def backnone():
     return redirect(url_for('page',pg=1))
 
 @app.route('/page/<int:pg>')
 def page(pg):
-    temp=get_db().execute('SELECT id, title, date, abstract FROM blog ORDER BY id DESC LIMIT 8 OFFSET ?',(pg*8-8,)).fetchall()
-    max_pg=((get_db().execute('SELECT count(*) FROM blog;').fetchall()[0][0]+7)/8) or 1
-    if pg > max_pg or pg < 1:
-        return render_template('error.html'), 404
+    cur=get_db().cursor()
+    cur.execute(' SELECT id, title, date, abstract FROM blog ORDER BY id DESC LIMIT 8 OFFSET ?',(pg*8-8,))
+    tem=cur.fetchall()
+    cur.execute('SELECT count(*) FROM blog;')
+    pmax=((cur.fetchall()[0][0]+7)/8 or 1)
+    if pg > pmax or pg < 1:
+        return render_template('error.html'), 500
     else:
-        return render_template('page.html',temp=temp,max_pg=max_pg,pg=pg)
+        return render_template('page.html',tem=tem,pmax=pmax,pg=pg)
 
 @app.route('/memo',methods=['GET', 'POST'])
 def memo():
     if request.method == 'POST':
-        if request.form['comment']:
+        if request.form['comment'] and session.get('angelina')!=50:
+                session['angelina']=(session.get('angelina') or 0)+1
                 author = request.form['author'] or u'匿名'
-                get_db().execute('insert into comm (content, author, blog) values (?, ?, ?)', (request.form['comment'], author, 0))
+                cur=get_db().cursor()
+                cur.execute('insert into comm (content, author, blog) values (?, ?, ?)', (request.form['comment'], author, 0))
                 get_db().commit()
                 return redirect(url_for('memo'))
     try:
-        temp=get_db().execute(' SELECT content, date, author, id FROM comm WHERE blog=0').fetchall()
-        temp.reverse()
+        cur=get_db().cursor()
+        cur.execute(' SELECT content, date, author, id FROM comm WHERE blog=0')
+        tem=cur.fetchall()
+        tem.reverse()
     except:
-        return render_template('memo.html',temp=[])
+        return render_template('memo.html',tem=[])
     else:
-        return render_template('memo.html',temp=temp)
+        return render_template('memo.html',tem=tem)
 
-@app.route('/article/<int:blog>', methods=['GET', 'POST'])
-def article(blog):
+@app.route('/article/<int:bg_id>', methods=['GET', 'POST'])
+def article(bg_id):
     if request.method == 'POST':
-        if request.form['comment']:
+        if request.form['comment'] and session.get('angelina')!=50:
+                session['angelina']=(session.get('angelina') or 0)+1
                 author = request.form['author'] or u'匿名'
-                get_db().execute('insert into comm (content, author, blog) values (?, ?, ?)', (request.form['comment'], author, blog))
+                cur=get_db().cursor()
+                cur.execute('insert into comm (content, author, blog) values (?, ?, ?)', (request.form['comment'], author, bg_id))
                 get_db().commit()
-                return redirect(url_for('article',blog=blog))
+                return redirect(url_for('article',bg_id=bg_id))
     try:
-        temp=get_db().execute('SELECT title, date, content from blog where id=?',(blog,)).fetchall()[0]
+        cur=get_db().cursor()
+        cur.execute('SELECT title, date, content from blog where id=?',(bg_id,))
+        cont=cur.fetchall()[0]
     except:
         return render_template('error.html'), 404
     else:
-        tem=get_db().execute(' SELECT content, date, author, id FROM comm WHERE blog=?',(blog,)).fetchall() or [('快来发布第一条评论吧','',''),]
-        return render_template('article.html',t=temp[0],d=temp[1],c=temp[2],id=blog,tem=tem,back=request.referrer)
+        cur.execute(' SELECT content, date, author, id FROM comm WHERE blog=?',(bg_id,))
+        tem=cur.fetchall() or [('快来发布第一条评论吧','',''),]
+        return render_template('article.html',t=cont[0],d=cont[1],c=cont[2],id=bg_id,tem=tem,back=request.referrer)
+
+@app.route('/del/<int:bg_id>/<int:ccid>')
+def delet(bg_id,ccid):
+    if session.get('log'):
+        try:
+            cur=get_db().cursor()
+            cur.execute('DELETE FROM comm WHERE id = ? ',(ccid,))
+            get_db().commit()
+        finally:
+            if not bg_id== 0 :
+                return redirect(url_for('article',bg_id=bg_id))
+            else:
+                return redirect(url_for('memo'))
+    return redirect(url_for('article',bg_id=bg_id))
+
 
 @app.route('/arch<int:arc>/<int:pg>')
 def arch(arc,pg):
-    temp=get_db().execute(' SELECT id, title, date, abstract FROM blog WHERE file=? ORDER BY id DESC LIMIT 8 OFFSET ?',(arc,pg*8-8)).fetchall()
-    max_pg=(get_db().execute('SELECT count(*) FROM blog;').fetchall()[0][0]+7)/8
-    if pg > max_pg or pg < 1:
-        return render_template('error.html'), 404
+    cur=get_db().cursor()
+    cur.execute(' SELECT id, title, date, abstract FROM blog WHERE file=? ORDER BY id DESC LIMIT 8 OFFSET ?',(arc,pg*8-8))
+    tem=cur.fetchall()
+    cur.execute('SELECT count(*) FROM blog;')
+    pmax=(cur.fetchall()[0][0]+7)/8
+    if pg > pmax or pg < 1:
+        return render_template('error.html'), 500
     else:
-        return render_template('page.html',temp=temp,max_pg=max_pg,pg=pg)
+        return render_template('page.html',tem=tem,pmax=pmax,pg=pg)
 
 
 @app.route('/robots.txt')
 def robots():
     return send_from_directory(app.static_folder,'robots.txt')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
