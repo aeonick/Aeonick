55efc2bdacf4439fc32e67e08831177c6b99c567
unknown <sky101001@gmail.com>
2016-08-23 18:37:00 +0800

    'edit'

--- aids.py
+++ aids.py
@@ -8,14 +8,14 @@ def check(pasword):
     else:
         pasword += '1396'
     m.update(pasword)
-    if m.hexdigest()=='24f1dfebc07150ade8e1dee20b02b942':
+    if m.hexdigest()=='9bd414e8c3cdd92cc9df53cc5a2f98dc':
         return True
     else:
         return False
 
 def abstr(text):
     sta=0
-    while sta<len(text) and sta < 180:
+    while sta<len(text) and sta < 160:
         if text[sta] == '<':
             end = sta
             while not text[end] == '>':
@@ -29,5 +29,24 @@ def abstr(text):
                 text=text[0:sta]+text[end+1:]
             sta -= 1
         sta += 1
-    text = text[:160]+'...'
+    tem=min(138,len(text))
+    leng=len(text)-1
+    while True:
+        tem -= 1
+        if tem < 60 or text[tem] == '>' :
+            text = text[:138]+'...'
+            break
+        elif text[tem:tem+5] == '&nbsp' and tem > 130:
+            text=text[:tem+5]+'...'
+            break
+        elif text[tem:tem+4] == '<img':
+            rew = -1
+            while tem < leng:
+                tem += 1
+                if text[tem] == '\"':
+                    rew += 1
+                    if rew:
+                        text=text[:tem]+'\" style=\"width: 90px\">...'
+                        break
+            break
     return text
