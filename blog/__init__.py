dafddaea25d03127b48e76d6f92a0629a295f592
Colin <admin@skyin.win>
2019-01-22 21:13:22 +0800

    '项目重构'

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY") or "1"
 app = Flask(__name__)
 DATABASE = os.path.join(app.root_path, 'db.db')
 app.config.from_object(__name__)
